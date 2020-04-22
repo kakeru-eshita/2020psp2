7462b3a0c881a95214448d82dfdea54c1caed3bc
kakeru-eshita <bb35319010@ms.nagasaki-u.ac.jp>
2020-04-22 15:41:24 +0900

    ソースコード完成　説明まだ

--- k00/k00.c
+++ k00/k00.c
@@ -5,6 +5,7 @@
 int main(void)
 {
     double max_val=0,min_val=200;
+    int gen;
     double val;
     char fname[FILENAME_MAX];
     char buf[256];
@@ -20,9 +21,11 @@ int main(void)
         fputs("File open error\n",stderr);
         exit(EXIT_FAILURE);
     }
+    
+    fgets(buf,sizeof(buf),fp);
 
     while(fgets(buf,sizeof(buf),fp) != NULL){
-        sscanf(buf,"%lf",&val);
+        sscanf(buf,"%d, %lf",&gen,&val);
         if(val>max_val){
             max_val = val;
         }
