13053ac778327889cb818a0599a33eeab4cdddd2
kakeru-eshita <bb35319010@ms.nagasaki-u.ac.jp>
2020-07-30 13:02:38 +0900

    提出２

--- k04/k04.c
+++ k04/k04.c
@@ -2,22 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#define ID_NUM 14
 
 struct all_data{
     int ID;
     int gender;
     double height;
 }
-struct all_data data1;
 
 int main(void)
-{
-    int ID, i=0, j=0;
+{　
+    int ID, i=0,j=0;
     char fname[FILENAME_MAX];
     char buf[256],buf2[256];
     FILE* fp;
     FILE* fp2;
-    
+    struct all_data data1[ID_NUM];
+
     printf("input the filename of sample height :");
     fgets(fname,sizeof(fname),stdin);
     fname[strlen(fname)-1] = '\0';
@@ -30,27 +31,53 @@ int main(void)
     printf("input the filename of sample ID :");
     fgets(fname,sizeof(fname),stdin);
     fname[strlen(fname)-1] = '\0';
-    fp = fopen(fname,"r");
-    if(fp==NULL){
+    fp2 = fopen(fname,"r");
+    if(fp2==NULL){
         fputs("No data\n",stderr);
         exit(EXIT_FAILURE);
     }
 
-       
+    i=0;
+    while(fgets(buf,sizeof(buf),fp)!=NULL){
+        sscanf(buf,"%d,%lf",&data1[i].gender,&data1[i].height);
+        i++;
+    }
+
+    i=0;
+    while(fgets(buf2,sizeof(buf),fp)!=NULL){
+        sscanf(buf2,"%d",&data1[i].ID);
+        i++;
     }
 
     if(fclose(fp) == EOF){
         fputs("file close error\n",stderr);
         exit(EXIT_FAILURE);
     }
+    if(fclose(fp2) == EOF){
+        fputs("file close error\n",stderr);
+        exit(EXIT_FAILURE);
+    }
 
-    
-    printf("---\n");
-    printf("ID : %d\n",ID);
-    printf("gender : %d\n",);
-    printf("height : %d\n",);
-    printf("、、、\n");
+    printf("ID:");
+    sscanf("%d",&ID);
 
+    for(i=0;i<14;i++){
+        if(ID==data1[i].ID){
+            printf("%d\n",data1[1].ID);
+        }
+        if(data1[i].gender==1){
+            printf("Male\n");
+        }
+        else{
+            printf("Female\n");
+        }
+        printf("%.1lf\n",data1[i].height);
+        j++;
+    }
+
+if(j==0){
+printf("No data");
+}
     return 0;
 }
 
